d3ed100488db406931ef7537cb96fa6749ddfcf2
AnnaBinoid <119959206+AnnaBinoid@users.noreply.github.com>
2023-11-08 17:58:23 +0300

    Add class Number and test class TestNumber

---
+++ 03hw/TestNumber.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+import seminars.third.hw.Number;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestNumber {
+
+    @Test
+    void testIsEven(){
+        int n = 8;
+        boolean result = Number.evenOddNumber(n);
+        assertTrue(result);
+    }
+
+    @Test
+    void testIsOdd(){
+        int n = 9;
+        boolean result = Number.evenOddNumber(n);
+        assertFalse(result);
+    }
+
+    @Test
+    void testInInterval(){
+        int n = 25;
+        boolean result = Number.numberInInterval(n);
+        assertTrue(result);
+    }
+
+    @Test
+    void testNotInInterval(){
+        int n = 101;
+        boolean result = Number.evenOddNumber(n);
+        assertFalse(result);
+    }
+
+}
